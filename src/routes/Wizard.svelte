1f8daaad7e71ae277d85501c754a9bad10c1878b
Maarten Waanders <maarten@phbs.eu>
2024-02-14 08:34:58 +0100

    Fix formatting onblur

--- src/routes/Wizard.svelte
+++ src/routes/Wizard.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
-  let value = 0;
+  let amount = "0";
+
+  function applyFormatting() {
+    amount = new Intl.NumberFormat("nl-NL", {
+      style: "currency",
+      currency: "EUR",
+    }).format(Number(amount));
+  }
 </script>
 
 <div class="wizard">
   <h2>Welk bedrag staat er op dit moment open?</h2>
   <input
-    on:change={() =>
-      new Intl.NumberFormat("nl-NL", {
-        style: "currency",
-        currency: "EUR",
-      }).format(value)}
+    bind:value={amount}
+    on:blur={applyFormatting}
     type="text"
     name="currency-field"
     id="currency-field"
-    {value}
     data-type="currency"
     placeholder="€50.000,00"
   />
